51d56da9cad78bae23b5e9542e97015c8aecfd32
Ian Maleney <imaleney@gmail.com>
2019-05-30 16:58:23 +0100

    Server-Side Messaging

--- index.js
+++ index.js
@@ -3,21 +3,49 @@ const app = express();
 const port = 3000;
 const { spawn } = require("child_process");
 const bodyParser = require("body-parser");
+const EventEmitter = require("events");
 app.use(bodyParser.json());
 app.use(express.static("static"));
 
 var state = {
-  first_time: true
+  first_time: true,
+  ready: true,
+  open: true,
+  freq: 0,
+  message: ""
 };
+
+class SetOpenState extends EventEmitter {}
+const setOpenState = new SetOpenState();
+
+setOpenState.on("access", () => {
+  setTimeout(() => {
+    state.open = true;
+    console.log("Open to new requests");
+  }, 10000);
+});
+
+setOpenState.on("ready", () => {
+  setTimeout(() => {
+    state.ready = true;
+    console.log("Ready");
+  }, 2000);
+});
 
 const play = async function(req, res) {
   var freq = await req.body.freq.toString();
   var sdr = await spawn("bash", [__dirname + "/sdr.sh", freq]);
 
+  state.freq = freq;
+  state.ready = false;
+  state.open = false;
+  console.log("Closed to Requests");
+
   sdr.stderr.on("data", data => {
     console.log(`stderr: ${data}`);
     if (data.includes("Output at")) {
-      res.send("ready");
+      state.message = "ready";
+      res.send(state);
     }
   });
 
@@ -26,17 +54,37 @@ const play = async function(req, res) {
   });
 };
 
+app.get("/state", (req, res) => {
+  res.send(state);
+});
+
+app.post("/state", (req, res) => {
+  var playing = req.body.ready;
+  if (playing) {
+    setOpenState.emit("ready");
+    setOpenState.emit("access");
+  }
+  res.send(state);
+});
+
+const check_if_ready = function(open_state, req, res) {
+  if (open_state) {
+    console.log("Ready to Play");
+    play(req, res);
+  } else {
+    console.log("Not Ready Yet");
+    setTimeout(() => {
+      check_if_ready(state.open, req, res);
+    }, 1000);
+  }
+};
+
 app.post("/radio", (req, res) => {
   const kill = spawn("bash", [__dirname + "/kill.sh"]);
   kill.on("close", code => {
-    if (state.first_time) {
-      res.setHeader("Content-Type", "text/html");
-    }
     if (code === 0 || state.first_time) {
       state.first_time = false;
-      setTimeout(() => {
-        play(req, res);
-      }, 100);
+      check_if_ready(state.open, req, res);
     } else {
       res.send("Failed to kill the rtl_fm process.");
       console.log("Failed to kill the rtl_fm process.");
---
+++ static/main.js
@@ -0,0 +1,204 @@
+let body = document.body;
+let b = document.querySelector("#tune");
+let a = document.querySelector("audio");
+let s = document.querySelector("input");
+let p = document.querySelector("#slider-value");
+const stream_url = "http://206.189.18.163:8000/vorbis";
+
+let svgString = '<svg><path d="M10,10 L50,100 L90,50"></path></svg>';
+
+// Events
+var updatePlayState = new Event("update_play_state");
+var updateFrequency = new Event("update_frequency");
+var updateStarted = new Event("update_started");
+
+// State
+let prevState = {};
+let state = {
+  frequency: "",
+  playState: "off",
+  started: false
+};
+
+// Utilities
+const dispatchPlayState = function(
+  new_playstate,
+  el = b,
+  callback = updatePlayState
+) {
+  el.dataset.playstate = new_playstate;
+  body.dispatchEvent(callback);
+};
+
+const dispatchFrequency = function(
+  new_freq,
+  el = s,
+  callback = updateFrequency
+) {
+  console.log(new_freq);
+  el.dataset.frequency = new_freq;
+  el.value = new_freq;
+  body.dispatchEvent(callback);
+};
+
+// Update Button Text
+const updateButton = function(button_state) {
+  switch (button_state) {
+    case "off":
+      b.textContent = "Tune In";
+      b.disabled = false;
+      break;
+    case "closed":
+      b.textContent = "Hang On";
+      b.disabled = true;
+      break;
+    case "retuned":
+      b.textContent = "Retune";
+      b.disabled = false;
+      break;
+    case "tuning":
+      b.textContent = "Tuning...";
+      b.disabled = true;
+      break;
+    case "playing":
+      b.textContent = "Pause";
+      b.disabled = false;
+      break;
+    case "paused":
+      b.textContent = "Play";
+      b.disabled = false;
+      break;
+    default:
+      b.textContent = "Tune In";
+      b.disabled = false;
+      break;
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  body.dispatchEvent(updateFrequency);
+});
+
+body.addEventListener("update_started", e => {
+  if (state.started) return;
+  state.started = true;
+});
+
+body.addEventListener("update_play_state", e => {
+  let new_play_state = b.dataset.playstate;
+  prevState = state;
+  state.playState = new_play_state;
+  updateButton(new_play_state);
+});
+
+body.addEventListener("update_frequency", e => {
+  let new_frequency = s.dataset.frequency;
+  prevState = state;
+  state.frequency = new_frequency;
+  console.log({ state });
+  p.textContent = `${new_frequency}Mhz`;
+});
+
+a.addEventListener("loadeddata", () => {
+  console.log("loadeddata");
+  let data = {
+    ready: true
+  };
+  fetch("/state", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    cache: "no-store",
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(server_state => {
+      console.log({ server_state });
+      setTimeout(() => {
+        dispatchPlayState("playing");
+      }, 5000);
+    });
+});
+
+a.addEventListener("ended", () => {
+  console.log("ended");
+  dispatchPlayState("tuning");
+  fetch("/state")
+    .then(res => res.json())
+    .then(server_state => {
+      console.log(server_state);
+      if (server_state.ready) {
+        a.src = null;
+        a.src = stream_url;
+        a.play();
+        dispatchFrequency(server_state.freq);
+      }
+    });
+});
+
+s.addEventListener("change", () => {
+  dispatchFrequency(s.value);
+  if (state.started) {
+    dispatchPlayState("retuned");
+  }
+});
+
+const start = function() {
+  dispatchPlayState("tuning");
+  let data = {
+    freq: state.frequency
+  };
+
+  fetch(`/radio`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    cache: "no-store",
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(server_state => {
+      dispatchFrequency(server_state.freq);
+      console.log(server_state);
+      if (server_state.message === "ready") {
+        if (!state.started) body.dispatchEvent(updateStarted);
+        a.src = null;
+        a.src = stream_url;
+        a.play();
+        dispatchPlayState("closed");
+      }
+    });
+};
+
+const pause = function() {
+  dispatchPlayState("paused");
+  a.muted = true;
+};
+
+const play = function() {
+  dispatchPlayState("playing");
+  a.muted = false;
+};
+
+b.addEventListener("click", () => {
+  switch (state.playState) {
+    case "off":
+      start();
+      break;
+    case "playing":
+      pause();
+      break;
+    case "paused":
+      play();
+      break;
+    case "retuned":
+      start();
+      break;
+
+    default:
+      pause();
+      break;
+  }
+});
